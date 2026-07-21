6fd61e278d7f2470147897fe523e2ff6f3869bf5
NIO_81720 <nadiasparks843@gmail.com>
2026-07-21 12:02:59 +0000

    Improved formatting

--- restaurant-receipt.py
+++ restaurant-receipt.py
@@ -1,13 +1,15 @@
 print("NIO'S Restaurant")
+print("-----------------")
 print("Nadia ")
-print(" Water" \
-" Chips" \
+print("Water " \
+"Chips  " \
 "Chicken")
 
-print("Ghc 5.00" \
-"Ghc 50.00" \
-"Ghc 80.00")
+print("Ghc5.00  " \
+"Ghc50.00  " \
+"Ghc80.00")
 
+print("       ")
 print("Total Amount:Ghc 135.00")
 print("Thank You" \
 "(>_<)")
