a6a39e73f8a4629d94127b66c947e3b57c7645eb
NIO_81720 <nadiasparks843@gmail.com>
2026-07-21 11:34:52 +0000

    First commit

---
+++ my_introduction.py
@@ -0,0 +1,11 @@
+#This program is to showcase;
+#Using the print fuction
+#Adding comments to my code.
+
+print("Nadia Ibrahim Olabimpe")
+print("IT")
+print("100")
+print("Accra")
+print("Not entirely sure yet")
+print("To learn python and use it to solve problems and to apply the knowlede gained in other programming languages")
+print("I'm yet to figure that out.")
