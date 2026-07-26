b94e9a17c472308fb29a1dc965c9f36ab87a5e02
NIO_81720 <nadiasparks843@gmail.com>
2026-07-26 20:22:26 +0000

    Mission-3

---
+++ Assignments/student-registration-portal.py
@@ -0,0 +1,73 @@
+#Mission 3: Student Registration Portal (Phase II)
+
+## Your Final Output Should Include:
+
+# * Full Name
+# * Student ID
+# * Programme
+# * Level
+# * Age
+# * University Email
+# * Phone Number
+# * Favourite Programming Language
+# * Dream Career
+# * Hall of Residence
+# * Emergency Contact
+# * Hobby
+# * Nationality
+
+# Your output should be neat, professional, and easy to read.
+
+print("_____________________UNIVERSITY OF GHANA________________________")
+print("                    ")
+
+print("Welcome To the Student Registeration Portal")
+print("-------------------------------------------")
+
+full_name= input("Full Name: ")
+student_ID = input("Student ID: ")
+program= input("Program of Study: ")
+level = input("Level: ")
+age = input("How old are you?: ")
+email = input("UG Email: ")
+phone_number = input("Active phone number: ")
+favourite_programming_language = input("What is your favourite programming language?")
+dream_career = input("Your dream career?: ")
+hall = input("Hall of Residence: ")
+emergency_contact = input("Emergency Contact Info: ")
+hobby = input("What is your hobby?: ")
+nationality = input("Your nationality: ")
+
+print(" ")
+
+print("Thank you for submitting your details!")
+
+print("   ")
+print("   ")
+
+print("=============================")
+print("= Complete Student Profile. =")
+print("=============================")
+print("  ")
+print("Welcome "+ full_name + " !")
+print("Kindly check and verify your details.")
+
+print(" ")
+
+print( "Full Name: " + full_name)
+print( "Student ID:  " + student_ID)
+print( "Program of study: " + program)
+print( "Level: " + level)
+print( "How old are you?: " + age)
+print( "UG Email: " + email)
+print( "Active phone number: " + phone_number)
+print( "Favourite Programming Language: " + favourite_programming_language)
+print( "Dream Career: " + dream_career)
+print( "Hall of Residence: " + hall)
+print( "Emergency Contact Info: " + emergency_contact)
+print( "Hobby: " + hobby)
+print( "Nationality: " + nationality)
+
+print("")
+
+print("Small steps forward still move you closer to your dreams")
